5c7b95e3e1cc20dc1e1deb8dddd3e20784dfdea8
chanakard <chanakaroshan86@gmail.com>
2020-06-14 04:55:32 +0530

    Up to 129 Videos all committed

--- angular-bookstore/src/app/components/checkout/checkout.component.ts
+++ angular-bookstore/src/app/components/checkout/checkout.component.ts
@@ -1,3 +1,5 @@
+import { CartService } from './../../services/cart.service';
+import { CartItem } from 'src/app/common/cart-item';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 
@@ -9,9 +11,16 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class CheckoutComponent implements OnInit {
 
   checkOutFormGroup:FormGroup;
-  constructor(private _formBuilder:FormBuilder) { }
+
+  cartItems:CartItem[]=[];
+  totalPrice:number=0;
+
+  constructor(private _formBuilder:FormBuilder,private _cartService:CartService) { }
 
   ngOnInit(): void {
+
+    this.cartDetails();
+
     this.checkOutFormGroup = this._formBuilder.group({
       customer : this._formBuilder.group({
         firstName :[''],
@@ -31,6 +40,14 @@ export class CheckoutComponent implements OnInit {
         state:[''],
         country:[''],
         zipcode:['']
+      }),
+      creditCard: this._formBuilder.group({
+        cardType:[''],
+        nameOnCard:[''],
+        cardNumber:[''],
+        cvv:[''],
+        expirationMonth:[''],
+        expirationYear:['']
       })
     })
   }
@@ -42,5 +59,26 @@ export class CheckoutComponent implements OnInit {
     console.log(this.checkOutFormGroup.get('customer').value.firstName);
   }
 
-  copyShippingAddressToBillingAddress
+  copyShippingAddressToBillingAddress(event)
+  {
+      if(event.target.checked)
+      {
+        this.checkOutFormGroup.controls.billingAddress.setValue(this.checkOutFormGroup.controls.shippingAddress.value);
+      }
+      else
+      {
+        this.checkOutFormGroup.controls.billingAddress.reset();
+      }
+  }
+
+
+  cartDetails()
+  {
+    this.cartItems = this._cartService.cartItems;
+    this._cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    )
+
+    this._cartService.calculateTotalPrice();
+  }
 }
